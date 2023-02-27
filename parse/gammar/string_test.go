c16b5115e33802c8c426fa12908631591c5fe591
halprin <halprin@users.noreply.github.com>
2023-02-26 20:51:37 -0600

    More tests for the String grammar using EOF

--- parse/gammar/string_test.go
+++ parse/gammar/string_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/halprin/fhirpath/lex"
 	"github.com/halprin/fhirpath/parse"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"testing"
 )
 
@@ -38,6 +39,40 @@ func TestStringWithQuoteInIt(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, "I like ' in my strings", stringGrammar.Value)
+}
+
+func TestStringFailsWithUnfinishedString(t *testing.T) {
+	tokenBuffer := &parse.TokenBuffer{
+		Lexer: lex.NewLexer("'beginning quote but no ending one"),
+	}
+
+	_, err := NewString(tokenBuffer)
+	assert.Equal(t, io.EOF, err)
+
+	nextToken, err := tokenBuffer.Pop()
+	assert.NoError(t, err)
+
+	assert.Equal(t, lex.Token{
+		Type:    lex.QUOTE,
+		Literal: '\'',
+	}, nextToken)
+}
+
+func TestStringIsNotAString(t *testing.T) {
+	tokenBuffer := &parse.TokenBuffer{
+		Lexer: lex.NewLexer("26.45"),
+	}
+
+	_, err := NewString(tokenBuffer)
+	assert.Equal(t, parse.NoGrammarParse, err)
+
+	nextToken, err := tokenBuffer.Pop()
+	assert.NoError(t, err)
+
+	assert.Equal(t, lex.Token{
+		Type:    lex.NUMERIC,
+		Literal: '2',
+	}, nextToken)
 }
 
 //TODO: support needs to be added for escapes
