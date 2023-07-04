a282dc0388a70ff681d27139a5a08cfc94626891
halprin <halprin@users.noreply.github.com>
2023-07-03 19:09:42 -0600

    Initial stab at InvocationExpression

--- engine/invocationExpression.go
+++ engine/invocationExpression.go
@@ -2,9 +2,14 @@ package engine
 
 import (
 	"github.com/halprin/fhirpath/grammar"
+	"github.com/halprin/rangechain"
 )
 
 func (receiver *engine) InvocationExpression(fhir map[string]interface{}, node grammar.Tree) (interface{}, error) {
-	//TODO: call future things
-	return []string{"male"}, nil
+	return rangechain.FromSlice(node.Children()).ReduceWithInitialValue(func(accumulatorInterface interface{}, currentChildInterface interface{}) (interface{}, error) {
+		accumulator := accumulatorInterface.(map[string]interface{})
+		currentChild := currentChildInterface.(grammar.Tree)
+
+		return receiver.Execute(accumulator, currentChild)
+	}, fhir)
 }
