ec8e74450373d6dfbcb679c4ffa3a63220bb97e0
halprin <halprin@users.noreply.github.com>
2023-07-13 16:25:43 -0600

    Add code documentation to the top-level Evaluate function

--- evaluate.go
+++ evaluate.go
@@ -6,8 +6,11 @@ import (
 	"github.com/halprin/fhirpath/internal/grammar"
 )
 
+// Evaluate evaluates the FHIR path against the supplied FHIR JSON.
+// Returns a slice of values and an optional error.  If the evaluation resulted in nothing, an empty slice is returned.  A slice of size 1 or larger is possible depending on whether the evaluation matched multiple values.
+// This function is a generic function, so it takes a type parameter.  Upon evaluation, any results that are not the same as the type parameter are filtered out.  If you want nothing potentially filtered out, use `any` as the type paramter.
 func Evaluate[T any](fhirString string, fhirPath string) ([]T, error) {
-	
+
 	fhir, err := unmarshalFhir(fhirString)
 	if err != nil {
 		return nil, err
