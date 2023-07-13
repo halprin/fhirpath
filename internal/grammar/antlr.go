ce7478f8cb9179325c8a62b438efe2a58e9152ac
halprin <halprin@users.noreply.github.com>
2023-07-13 16:36:00 -0600

    Document antlr.go

--- internal/grammar/antlr.go
+++ internal/grammar/antlr.go
@@ -5,7 +5,9 @@ import (
 	"github.com/halprin/fhirpath/internal/grammar/antlrGen"
 )
 
+// CreateTree is the top-level function to parse a FHIR path into a `Tree`.
 func CreateTree(fhirPath string) (Tree, error) {
+	//in this specific case, the ANTLR library is used to parse the FHIR path
 	input := antlr.NewInputStream(fhirPath)
 	lexer := antlrGen.NewFhirpathLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
