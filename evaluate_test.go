23939feebd889904d99f740b951bcaad6492cffe
halprin <halprin@users.noreply.github.com>
2023-07-15 16:05:07 -0600

    Benchmarking

--- evaluate_test.go
+++ evaluate_test.go
@@ -2,8 +2,9 @@ package fhirpath
 
 import (
 	_ "embed"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 //go:embed sample/patient.json
@@ -28,6 +29,14 @@ func TestEvaluate_Where_NotEqual(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Contains(t, result, "S99955754")
+}
+
+func TestEvaluate_Where_Other(t *testing.T) {
+	result, err := Evaluate[string](fhirBundleOrder, "Bundle.entry.where(resource.resourceType='ServiceRequest').resource.code.coding.code")
+
+	assert.NoError(t, err)
+	assert.Contains(t, result, "54089-8")
+	assert.Contains(t, result, "57717-1")
 }
 
 func TestEvaluate_Index(t *testing.T) {
