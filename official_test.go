dea25e8d470b3db023f24d23e470ac34168e454e
halprin <halprin@users.noreply.github.com>
2023-07-31 20:17:22 -0600

    RUns through all the official tests and executes them but without the FHIR

--- official_test.go
+++ official_test.go
@@ -3,6 +3,7 @@ package fhirpath
 import (
 	_ "embed"
 	"encoding/xml"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,19 +13,21 @@ import (
 var officialTestXmlSpec []byte
 
 type OfficialTests struct {
-	Groups []OfficialTestGroup
+	XMLName xml.Name            `xml:"tests"`
+	Groups  []OfficialTestGroup `xml:"group"`
 }
 
 type OfficialTestGroup struct {
-	Name  string
-	Tests []OfficialTest
+	Name  string         `xml:"name,attr"`
+	Tests []OfficialTest `xml:"test"`
 }
 
 type OfficialTest struct {
-	Name       string
-	Expression string
-	InputFile  string
-	Outputs    []string
+	Name       string `xml:"name,attr"`
+	Expression string `xml:"expression"`
+	InputFile  string `xml:"inputfile,attr"`
+	//	Invalid    bool     `xml:"expression>invalid,attr"`
+	Outputs []string `xml:"output"`
 }
 
 func TestOfficial(t *testing.T) {
@@ -33,9 +36,15 @@ func TestOfficial(t *testing.T) {
 	err := xml.Unmarshal(officialTestXmlSpec, &officialTests)
 	assert.NoError(t, err)
 	//for each test, call officialTestTemplate(...) and then call t.Run() the return value
+	for _, group := range officialTests.Groups {
+		for _, test := range group.Tests {
+			testName := fmt.Sprintf("%s/%s", group.Name, test.Name)
+			t.Run(testName, officialTestTemplate(test.Expression, "", test.Outputs))
+		}
+	}
 }
 
-func officialTestTemplate(testName string, fhirPath string, fhir string, expectedResult []string) func(*testing.T) {
+func officialTestTemplate(fhirPath string, fhir string, expectedResult []string) func(*testing.T) {
 	return func(t *testing.T) {
 		_, err := Evaluate[any](fhir, fhirPath)
 		if err != nil {
